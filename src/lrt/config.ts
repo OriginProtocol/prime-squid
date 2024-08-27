a6802e8538c2c6d242f6d39b0d3b4453faa17a9c
Chris Jacobs <apexearth@gmail.com>
2024-08-27 10:58:35 -0700

    push date back 1 day

--- src/lrt/config.ts
+++ src/lrt/config.ts
@@ -10,7 +10,7 @@ const primeLaunchDate = new Date('2024-02-05 12:00 PM PST')
 
 const eth = (val: bigint) => val * 1_000000000_000000000n
 
-export const xpEndTimestamp = 1724785200000 // 2024-08-27 12:00 PM PST
+export const xpEndTimestamp = 1724871600000 // 2024-08-28 12:00 PM PST
 
 export interface PointCondition {
   name: string
